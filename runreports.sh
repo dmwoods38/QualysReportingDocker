1e3e9139e118f3e204a6438ba37c880c5e4382bf
Dean Woods <dmwoods38@gmail.com>
2016-04-01 12:17:44 -0400

    Removed DB dependencies.

--- runreports.sh
+++ runreports.sh
@@ -4,14 +4,6 @@ PYTHONBIN=/usr/bin/python
 QGREPORTS=/usr/local/lib/python2.7/dist-packages/qgreports
 QGSCRIPTS=${QGREPORTS}/scripts
 GETREPORTSFILE=${QGSCRIPTS}/get_reports.py
-PGSQLSERVICE=/etc/init.d/postgresql
 
 echo "Getting reports"
-${PGSQLSERVICE} start
-while ! netcat -vv localhost 5432
-do
-	echo "$(date) - waiting for DB to start"
-	sleep 3
-done
-echo "$(date) -connected to DB successfully"
 ${PYTHONBIN} ${GETREPORTSFILE}
