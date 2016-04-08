dc22dbc7d14530ff55434b5ac951322a95964b4f
StanPast <StanPast@users.noreply.github.com>
2016-04-08 11:55:41 -0700

    Update runreports.sh
    echo more

--- runreports.sh
+++ runreports.sh
@@ -5,5 +5,5 @@ QGREPORTS=/usr/local/lib/python2.7/dist-packages/qgreports
 QGSCRIPTS=${QGREPORTS}/scripts
 GETREPORTSFILE=${QGSCRIPTS}/get_reports.py
 
-echo "Getting reports"
+echo "RUNREPORTS.SH: Getting reports: get_reports.py"
 ${PYTHONBIN} ${GETREPORTSFILE}
